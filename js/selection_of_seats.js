54d0189dd4927bd353dc11afcae773bf7ff76e9c
Artyom Krasyuk <artem.krasyuk2004@gmail.com>
2025-04-25 21:58:02 +0400

    schema upgrade

--- js/selection_of_seats.js
+++ js/selection_of_seats.js
@@ -5,33 +5,70 @@ let numberOfSeatsCoupe = 36;
 let type = 'Смешанное купе';
 let array = type.split(' ');
 let newType = array[0] + '<br>' + array[1];
+let str1 = `<p class="carriage__number">12 вагон</p>`+
+    `<p class="carriage__seats">${numberOfSeatsCoupe} мест</p>`+
+    `<p class="carriage__price">2134 - 4234₽</p>`;
+let div1 = document.getElementById('coupe_info');
+div1.insertAdjacentHTML('beforeend', str1);
 if(numberOfSeatsCoupe % 4 == 0){
     for(let i = 1; i < numberOfSeatsCoupe; i+=4){
-        if(i == numberOfSeatsCoupe - 3) fillSchema(i, newType, true);
-        else fillSchema(i, newType, false);
+        if(i == numberOfSeatsCoupe - 3) fillCoupe(i, newType, true);
+        else fillCoupe(i, newType, false);
     }
 }
 
-function fillSchema(number, type, last){
+
+
+// response body
+let numberOfSeatsPlatzkart = 60;
+let str2 = `<p class="carriage__number">12 вагон</p>`+
+    `<p class="carriage__seats">${numberOfSeatsPlatzkart} мест</p>`+
+    `<p class="carriage__price">2134₽</p>`;
+let div2 = document.getElementById('platzkart_info');
+div2.insertAdjacentHTML('beforeend', str2);
+if(numberOfSeatsCoupe % 6 == 0){
+    for(let i = 1; i < numberOfSeatsPlatzkart; i+=6){
+        if(i == numberOfSeatsPlatzkart - 5) fillPlatzkart(i, true);
+        else fillPlatzkart(i, false);
+    }
+}
+
+
+
+// response body
+let numberOfSeatsSV = 20;
+let str3 = `<p class="carriage__number">12 вагон</p>`+
+    `<p class="carriage__seats">${numberOfSeatsSV} мест</p>`+
+    `<p class="carriage__price">4234₽</p>`;
+let div3 = document.getElementById('sv_info');
+div3.insertAdjacentHTML('beforeend', str3);
+if(numberOfSeatsSV % 2 == 0){
+    for(let i = 1; i < numberOfSeatsSV; i+=2){
+        if(i == numberOfSeatsSV - 1) fillSV(i, newType, true);
+        else fillSV(i, newType, false);
+    }
+}
+
+function fillCoupe(number, type, last){
     let add = '';
     if(last) add = 'last_coupe';
 
     let str = '<div class="coupe__wrapper">'+
                 `<div class="coupe ${add}">`+
                     '<div class="coupe__seats">'+
-                        '<div class="coupe__seat">'+
+                        '<div class="coupe__seat" data-tooltip="2134₽, верхнее">'+
                             '<img src="img/strela_up.png" alt="" class="seat__img_top">'+
                             `<p class="seat__number_top">${number+1}</p>`+
                         '</div>'+
-                        '<div class="coupe__seat">'+
+                        '<div class="coupe__seat" data-tooltip="2134₽, верхнее">'+
                                 '<img src="img/strela_up.png" alt="" class="seat__img_top">'+
                                 `<p class="seat__number_top">${number+3}</p>`+
                         ' </div>'+
-                        '<div class="coupe__seat">'+
+                        '<div class="coupe__seat" data-tooltip="4234₽, нижнее">'+
                             `<p class="seat__number_bottom">${number}</p>`+
                             '<img src="img/strela_down.png" alt="" class="seat__img_bottom">'+
                         '</div>'+
-                        '<div class="coupe__seat">'+
+                        '<div class="coupe__seat" data-tooltip="4234₽, нижнее">'+
                             `<p class="seat__number_bottom">${number+2}</p>`+
                             '<img src="img/strela_down.png" alt="" class="seat__img_bottom">'+
                         '</div>'+
@@ -39,6 +76,79 @@ function fillSchema(number, type, last){
                 '</div>'+
                 `<p class="coupe__type">${type}</p>`+
             '</div>';
-    let div = document.querySelector('.schema__coupe_row');
+    let div = document.getElementById('coupe_seats');
+    div.insertAdjacentHTML('beforeend', str);
+}
+
+function fillPlatzkart(number, last){
+    let add = '';
+    let addBottom = '';
+    if(last) {
+        add = 'last_coupe';
+        addBottom = 'last_coupe_bottom';
+    }
+
+    let str = '<div class="coupe__wrapper">'+
+                `<div class="coupe ${add}">`+
+                    '<div class="coupe__seats">'+
+                        '<div class="coupe__seat" data-tooltip="2134₽, верхнее">'+
+                            '<img src="img/strela_up.png" alt="" class="seat__img_top">'+
+                            `<p class="seat__number_top">${number+1}</p>`+
+                        '</div>'+
+                        '<div class="coupe__seat" data-tooltip="2134₽, верхнее">'+
+                                '<img src="img/strela_up.png" alt="" class="seat__img_top">'+
+                                `<p class="seat__number_top">${number+3}</p>`+
+                        ' </div>'+
+                        '<div class="coupe__seat" data-tooltip="2134₽, нижнее">'+
+                            `<p class="seat__number_bottom">${number}</p>`+
+                            '<img src="img/strela_down.png" alt="" class="seat__img_bottom">'+
+                        '</div>'+
+                        '<div class="coupe__seat" data-tooltip="2134₽, нижнее">'+
+                            `<p class="seat__number_bottom">${number+2}</p>`+
+                            '<img src="img/strela_down.png" alt="" class="seat__img_bottom">'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'+
+            '</div>';
+    let div = document.getElementById('platzkart_seats');
+    div.insertAdjacentHTML('beforeend', str);
+
+    let str1 = 
+    '<div class="coupe__wrapper">'+
+        `<div class="coupe_bottom ${addBottom}">`+
+            '<div class="coupe__seats_bottom">'+
+                '<div class="coupe__seat" data-tooltip="2134₽, нижнее">'+
+                    `<p class="seat__number_bottom">${number+4}</p>`+
+                    '<img src="img/strela_down.png" alt="" class="seat__img_bottom">'+
+            '</div>'+
+                '<div class="coupe__seat" data-tooltip="2134₽, верхнее">'+
+                    '<img src="img/strela_up.png" alt="" class="seat__img_top">'+
+                    `<p class="seat__number_top">${number+5}</p>`+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+    '</div>';
+    let div1 = document.getElementById('platzkart_bottom_seats');
+    div1.insertAdjacentHTML('beforeend', str1);
+}
+
+function fillSV(number, type, last){
+    let add = '';
+    if(last) add = 'last_coupe';
+
+    let str = '<div class="coupe__wrapper">'+
+                `<div class="coupe ${add}">`+
+                    '<div class="coupe__seats">'+
+                        '<div class="sv__seat" data-tooltip="4234₽">'+
+                            `<p class="seat__number_sv">${number}</p>`+
+                        '</div>'+
+                        '<div class="sv__seat" data-tooltip="4234₽">'+
+                                `<p class="seat__number_sv">${number+1}</p>`+
+                        ' </div>'+
+                    '</div>'+
+                '</div>'+
+                `<p class="coupe__type">${type}</p>`+
+            '</div>';
+    let div = document.getElementById('sv_seats');
     div.insertAdjacentHTML('beforeend', str);
 }
